b63c42a1732f2d973c5307dec7ed3a7490acd056
Bhowmik <sayantan.bhowmik@conocophillips.com>
2020-02-26 20:29:15 -0600

    Remove unnecessary matplotlib import
    Might add back later once we figure out how to see plots over ssh

--- 4_CNN_w_keras_frm_tf.py
+++ 4_CNN_w_keras_frm_tf.py
@@ -10,7 +10,6 @@ except:
     pass
 
 from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
